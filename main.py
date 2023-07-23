6ed533e62d6ad4df2489973b3b2107f5e661101a
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2023-07-23 15:26:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import datetime
 from Full_Length_code.fulllength import run_full_length
 from Half_Length_Code.halflength import run_half_length_code
 from accuracy_calculation.accuracycalculator import calculate_accuracy_percentage
-
+from remove_file.remove import remove
 def main():
     current_month = datetime.datetime.now().strftime("%B")
     st.title("BA Grooming  Accuracy Calculator")
 
     # Sidebar options
-    selected_option = st.sidebar.selectbox("Choose an option", ["Half Length","Full Length","Accuracy Calculator"])
+    selected_option = st.sidebar.selectbox("Choose an option", ["Half Length","Full Length","Accuracy Calculator","Existing File to Remove"])
 
     if selected_option == "Half Length":
         run_half_length_code(current_month)
@@ -22,5 +22,10 @@ def main():
         filename_half=f"accuracy_result_half_length_{current_month}.csv"
         filename_full=f"accuracy_result_full_length_{current_month}.csv"
         calculate_accuracy_percentage(filename_full,filename_half,current_month)
+    elif selected_option =="Existing File to Remove":
+        remove()
 if __name__ == "__main__":
     main()
+    # Footer with "Made with love" message centered and at the bottom
+    footer_message = f"<div style='position: relative; bottom: 10px; width: 100%; text-align: center;'>Made with ❤️ by Ashis Tiwari©. June 2023-{datetime.datetime.now().strftime('%B')} {datetime.datetime.now().year}</div>"
+    st.markdown(footer_message, unsafe_allow_html=True)
